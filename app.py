76aa2b28f3807e3d07ed8da2f2bb8dc5bce22dca
gunjasandeep <88416673+gunjasandeep@users.noreply.github.com>
2025-07-08 01:58:24 +0530

    Test

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
-print('Hello, world!')
+# Program to check if a number is prime or not
+
+num = 29
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# define a flag variable
+flag = False
+
+if num == 0 or num == 1:
+    print(num, "is not a prime number")
+elif num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+    # check if flag is True
+    if flag:
+        print(num, "is not a prime number")
+    else:
+        print(num, "is a prime number")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
